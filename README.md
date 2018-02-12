77b3c1d95f805170567a525e0906eb0900c3d81e
Andrew Zoka <mugendiz@gmail.com>
2018-02-12 17:24:05 -0500

    2nd commit in progress

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # hangman_CLI
-# hangman_CLI
+
+* **Letter.js**: Contains a constructor, Letter. Displays an underlying character or a blank placeholder, depending on whether or not the user has guessed the letter. That means the constructor should define:
+
+* **Word.js**: Contains a constructor, Word that depends on the Letter constructor. This creates an object representing the current word the user is attempting to guess.
+
+* **index.js**: The file containing the logic for the course of the game, which depends on `Word.js` 
--- letter.js
+++ letter.js
@@ -7,3 +7,28 @@
 //   * A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
 //
 //   * A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
+
+
+
+var Letter = function(underlyingCharacter, placeholder, guessedYet) {
+  this.underlyingCharacter: underlyingCharacter,
+  this.guessedYet: false,
+  this.placeholder: "_",
+  this.returnChar: returnChar();
+  this.charCheck: charCheck();
+};
+
+var returnChar = function() {
+  if (input === underlyingCharacter) {
+    console.log(underlyingCharacter);
+  } else
+    console.log(this.placeholder);
+};
+
+var charCheck function {
+  if (input === underlyingCharacter) {
+    console.log(this.guessedYe.true);
+  } else {
+    console.log(false);
+  }
+};
