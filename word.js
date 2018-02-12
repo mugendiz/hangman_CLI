238fe139b59ffcf8cac3a13e21ca5de9019656a8
Andrew Zoka <mugendiz@gmail.com>
2018-02-12 17:19:20 -0500

    2nd commit

---
+++ word.js
@@ -0,0 +1,31 @@
+// * **Word.js**: Contains a constructor, Word that depends on the Letter constructor. This is used to create an object representing the current word the user is attempting to guess. That means the constructor should define:
+//
+//   * An array of `new` Letter objects representing the letters of the underlying word.
+//
+// A function that returns a string representing the word.
+// This should call the function on each letter object (the first function defined in `Letter.js`) that displays the    character or an underscore and concatenate those together
+//
+//   * A function that takes a character as an argument and calls the guess function on each letter object (the second function defined in `Letter.js`)
+
+var letter = require("./letter.js)"
+
+  var Word = function() {
+    this.characterArray: characterArray,
+      this.wordToGuess: wordToGuess,
+      this.getChar: getChar
+  }
+}
+
+var = characterArray
+if (function() {
+  for (var i = 0; i < characterArray.length; i++) {
+    characterArray[i]);
+  } console.log(this.characterArray[]);
+
+  // var getWord = new Letter(this.underlyingCharacter[],
+  // }
+  //
+  // var = wordToGuess
+  // function() {
+  //   wordToGuess(charListCorrect[i]).toString()
+  // }
